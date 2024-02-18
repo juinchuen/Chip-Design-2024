5713fb2ff46e614f178ea18d5e90316c9c70714a
Donald Charles Carl III <dcc3637@moore.wot.eecs.northwestern.edu>
2024-02-18 14:52:02 -0600

    2/18

--- donny/ASIC_Project/fft.sv
+++ donny/ASIC_Project/fft.sv
@@ -4,107 +4,114 @@ module fft #(
     parameter D_WIDTH = 64,
     parameter LOG_2_WIDTH = 6
 ) (
-    input [((16 * D_WIDTH) - 1) : 0] input_sig,
+    input [15:0] input_sig_Re [((D_WIDTH) - 1):0],
+    input [15:0] input_sig_Im [((D_WIDTH) - 1):0],
     input clk, rst,
-    output [((16 * D_WIDTH) - 1) : 0] output_sig
+    output [15:0] output_sig_Re [((D_WIDTH) - 1):0]
+    output [15:0] output_sig_Im [((D_WIDTH) - 1):0]
 );
-    wire [((16 * D_WIDTH) - 1) : 0] seperated_sig;
+    // Correctly route the input signal
 
-
+    //wire [15:0] fft_data [((D_WIDTH) - 1):0]
     generate
+    
     for (genvar i = 0; i < D_WIDTH; i++) begin 
-        reg [LOG_2_WIDTH-1:0] ii;
-        reg [LOG_2_WIDTH-1:0] x;
-
-        initial begin
-            x = i;
-            ii = 0;
-            for (int j = 0; j < LOG_2_WIDTH; j++) begin
-                ii <<= 1;
-                ii |= (x & 1);
-                x >>= 1;
-            end
-        end
-
-        DFF_16Bit dff_instance (
-            .D(input_sig[(16*i + 15) -: 16]),
-            .clk(clk),
-            .rst(rst),
-            .Q(seperated_sig[(16*i + 15) -: 16])
-        );
+        genvar [LOG_2_WIDTH-1:0] ii;
+        genvar [LOG_2_WIDTH-1:0] x;
+          x = i;
+          ii = 0;
+          for (int j = 0; j < LOG_2_WIDTH; j++) begin
+              ii <<= 1;
+              ii |= (x & 1);
+              x >>= 1;
+          end
+        assign output_sig[ii] = input_sig[i];
     end
-    assign output_sig = seperated_sig;
-endgenerate
-
+  endgenerate
 endmodule
 
 module Butterfly#( 
     parameter D_WIDTH = 64,
     parameter LOG_2_WIDTH = 6
 ) (
-    input [((16 * D_WIDTH) - 1) : 0] input_sig,
+    input [15:0] input_Re, input_Im [((D_WIDTH) - 1):0],
     input start, clk, rst,
-    output [((16 * D_WIDTH) - 1) : 0] output_sig
+    output [15:0] output_Re, output_Im [((D_WIDTH) - 1):0]
 );
-  wire [((16 * D_WIDTH) - 1) : 0] reff_in, reff_out, imff_in, imff_out;
-  wire [(D_WIDTH - 1) : 0] count, stage, twiddle_index;
+  wire [15:0] reff_in [((D_WIDTH) - 1):0];
+  wire [15:0] reff_out [((D_WIDTH) - 1):0];
+  wire [15:0] imff_in [((D_WIDTH) - 1):0];
+  wire [15:0] imff_out [((D_WIDTH) - 1):0];
+  wire [15: 0] count, stage, twiddle_index;
+  wire twiddle_second;
   wire [8:0] re_twiddle, im_twiddle;
 
+  wire [15:0] curr_reg_Re, other_reg_Re, curr_reg_Im, other_reg_Im, new_Re, new_Im;
+
+  // Fix this to not interact with clock
   StageClock StageCount(.start(start), .shift(~(|count)), .rst(rst), .out(stage));
   // Might need to delay start for these two
+
   CountTo64 Counter(.start(start), .clk(clk), .rst(rst), .out(count));
   TwiddleFactorIndex TwiddleIndex(.stage(stage), .start(start), .clk(clk), .rst(rst), .out(twiddle_index));
+  assign twiddle_second = ~((count[5] & stage[0]) | (count[4] & stage[1]) | (count[3] & stage[2]) | (count[2] & stage[3]) | (count[1] & stage[4]) | (count[0] & stage[5]));
 
-  generate
-    for (genvar i = 0; i < D_WIDTH; i++) begin 
-      DFF_16Bit dff_instance (
-        .D(reff_in[(16*i + 15) -: 16]),
-        .clk(clk & ((i == count) | start)), // Prolly need to do this in a better way
-        .rst(rst),
-        .Q(reff_out[(16*i + 15) -: 16])
-      );
-
-      DFF_16Bit dff_instance (
-        .D(imff_in[(16*i + 15) -: 16]),
-        .clk(clk & ((i == count) | start)), // Prolly need to do this in a better way
-        .rst(rst),
-        .Q(imff_out[(16*i + 15) -: 16])
-      );
-    end
-  endgenerate
   //Get twiddle factor
   ReTwiddleMux ReTwiddleMux(.select(twiddle_index), .out(re_twiddle));
   ImTwiddleMux ImTwiddleMux(.select(twiddle_index), .out(im_twiddle));
-  
-  //
 
-// Apply_Twiddle UpdateRe(.upper_sig(), .lower_sigRe(), .lower_sigIm(), 
-//     .twiddle_factorRe(), .twiddle_factorIm(), .re_out(), .out());
+  //Get the output from the Twiddle factors
+  Apply_Twiddle Apply_Twiddle(.curr_reg_RE(curr_reg_Re), .other_reg_RE(other_reg_Re), .curr_reg_IM(curr_reg_Im), .other_reg_IM(other_reg_Im),
+    .twiddle_factorRe(re_twiddle), .twiddle_factorIm(im_twiddle), .twiddle_second(twiddle_second), .out_RE(new_Re), .out_IM(new_Im));
 
-  assign output_sig = ff_out;
-
+  //Handle all of the differnet values 
+  reg [15:0] Re_reg [((D_WIDTH) - 1):0];
+  reg [15:0] Im_reg [((D_WIDTH) - 1):0];
+  assign output_Re = Re_reg;
+  assign output_Im = Im_reg;
+  generate 
+    for (genvar i = 0; i < D_WIDTH; i++) begin 
+      assign output_Re[i] = Re_reg[i];
+      assign output_Im[i] = Im_reg[i];
+      always_ff @(negedge clk or negedge rst) begin
+        if (~rst) begin
+          Re_reg[i] <= 16'b0;
+          Im_reg[i] <= 16'b0;
+        end else if(start) begin
+          Re_reg[i] <= input_sig_Re[i];
+          Im_reg[i] <= input_sig_Im[i];
+        end else begin
+          Re_reg[i] <= (count == i) ? new_Re : Re_reg[i];
+          Im_reg[i] <= (count == i) ? new_Im : Im_reg[i];
+        end
+      end
+    end
+  endgenerate
 endmodule
 
 module Apply_Twiddle( 
     parameter D_WIDTH = 64,
     parameter LOG_2_WIDTH = 6
 ) (
-    input [(D_WIDTH - 1) : 0] upper_sig, lower_sigRe, lower_sigIm,
+    input [15:0] curr_reg_RE, other_reg_RE, curr_reg_IM, other_reg_IM,
     input [9:0] twiddle_factorRe, twiddle_factorIm,
-    input re_out,
-    output [((16 * D_WIDTH) - 1) : 0] out
+    input twiddle_second,
+    output [15:0] out_RE, out_IM
 );
-  wire [24:0] multi_inRe, multi_inIm, multi_outRe, multi_outIm, after_twiddle;
+  wire [24:0] multi_inRe, multi_inIm, add_inRe, add_inIm, RE_out_PreChop, IM_out_PreChop;
   
-  assign multi_inRe = re_out ? lower_sigRe : lower_sigIm;
-  assign multi_inIm = re_out ? lower_sigIm : lower_sigRe;
+  assign multi_inRe = twiddle_second ? other_reg_RE : curr_reg_RE;
+  assign multi_inIm = twiddle_second ? other_reg_IM : curr_reg_IM;
+  assign add_inRe = twiddle_second ? curr_reg_RE : other_reg_RE; 
+  assign add_inIm = twiddle_second ? curr_reg_IM : other_reg_IM;
 
-  assign multi_outRe = multi_inRe * lower_sigRe; 
-  assign multi_outIM = multi_inIm * lower_sigIm; 
+  assign RE_out_PreChop = (multi_inRe * twiddle_factorRe) - (multi_inIm * twiddle_factorIm) + add_inRe;
+  assign IM_out_PreChop = (multi_inRe * twiddle_factorIm) + (multi_inIm * twiddle_factorRe) + add_inIm;
   
+  assign out_RE = RE_out_PreChop[15:0];
+  assign out_IM = IM_out_PreChop[15:0];
   // This should really just be an adder where u invert the input and change carryout to 1
-  assign after_twiddle = re_out ? (multi_outRe - multi_outIM) : (multi_outRe + multi_outIM); 
-  assign out = after_twiddle + upper_sig; 
+  
 endmodule
 
 module CountTo64(
@@ -142,6 +149,7 @@ endmodule
 
 // endmodule
 
+
 module StageClock(
   input wire start, shift, rst,
   output wire [5:0] out
@@ -162,43 +170,7 @@ module StageClock(
 
 endmodule 
 
-module DFF_16Bit (
-  input wire [15:0] D, 
-  input wire clk, rst,
-  output wire [15:0] Q
-);
-  logic [15:0] data;
-  
-  always_ff @(negedge clk or negedge rst) begin
-    if (~rst) begin
-      data <= 16'b0;
-      // Qn <= 16'b1111111111111111;
-    end else begin
-      data <= D;
-    end
-  end
 
-  assign Q = data;
-endmodule
-
-module DFF_16Bit (
-  input wire [15:0] D, 
-  input wire clk, rst,
-  output wire [15:0] Q
-);
-  logic [15:0] data;
-  
-  always_ff @(negedge clk or negedge rst) begin
-    if (~rst) begin
-      data <= 16'b0;
-      // Qn <= 16'b1111111111111111;
-    end else begin
-      data <= D;
-    end
-  end
-
-  assign Q = data;
-endmodule
 
 module DFF_Bit (
   input wire D, clk, rst,
